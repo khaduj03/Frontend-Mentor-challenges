55eee0133f3d649075ef4614ec15529bc523ea6a
khadija <khadijawakili652@gmail.com>
2024-11-15 12:32:49 +0430

    Add result display page for mortgage calculations

--- mortgage-repayment-calculator-main/src/App.js
+++ mortgage-repayment-calculator-main/src/App.js
@@ -7,6 +7,9 @@ function App() {
   const [term, setTerm] = useState("");
   const [interest, setInterest] = useState("");
   const [mortgageType, setMortgageType] = useState("");
+  const [failedForm, setFailedForm]=useState(false)
+  const [result , setResult]=useState(null)
+  const [ total , setTotal]=useState(null)
 
   const handleInput = (setter) => (e) => {
     const value = e.target.value;
@@ -21,21 +24,44 @@ function App() {
     const amountNum = parseFloat(amount);
     const termNum = parseFloat(term) * 12; 
     const interestNum = parseFloat(interest) / 100;
-    let repyment=true
 
-    if (mortgageType === "repayment") {
+    if(!amount && !term && !interest && !mortgageType){
+      console.log("all is empty")
+    }
+    
+
+    if (mortgageType === "repayment" && amount && term &&interest &&mortgageType ) {
+      setFailedForm(true)
       const monthlyRate = interestNum / 12;
       const payment = (amountNum * monthlyRate * Math.pow(1 + monthlyRate, termNum)) / (Math.pow(1 + monthlyRate, termNum) - 1);
-      console.log(payment.toFixed(2));
-      console.log("You selected repayment");
-      repyment =true
-    } else {
+      const totalPayment=payment*termNum
+      let result=payment.toFixed(2)
+      setResult(result)
+      setTotal(totalPayment)
+    } 
+    
+    
+    else if(mortgageType==="interest" && amount && term &&interest &&mortgageType ) {
+      setFailedForm(true)
       const interestOnly = amountNum * (interestNum / 12);
-      console.log(interestOnly.toFixed(2));
-      console.log("You selected interest only");
-      repyment=false
+      let result=interestOnly.toFixed(2)
+      setResult(result)
+
+    }
+    
+    else{
+      return failedForm
     }
   };
+
+
+  const handleClear=()=>{
+    setAmount("")
+    setInterest("")
+    setMortgageType(null)
+    setTerm("")
+    setFailedForm(false)
+  }
 
   return (
     <div className="flex flex-col bg-[rgba(227,243,251,255)] justify-center items-center w-full h-screen">
@@ -43,14 +69,14 @@ function App() {
         <div className="w-full lg:w-1/2 flex flex-col p-6">
           <div className="flex justify-between">
             <h1 className="font-sans font-bold text-[rgba(19,48,64,255)]">Mortgage Calculator</h1>
-            <a href="#" className="font-sans text-sm">Clear All</a>
+            <p onClick={handleClear} className=" font-sans text-sm cursor-pointer">Clear All</p>
           </div>
 
           <form>
             <div className="h-[70px]">
               <label htmlFor="amount" className="text-xs text-[#437087]">Mortgage Amount</label>
               <div className='mt-[7px] focus-within:bg-[rgba(217,219,46,255)] relative bg-[rgba(227,243,251,255)] w-full rounded flex justify-between h-[35px] pb-[12px] pt-[5px] items-center border-[1.2px] focus-within:border-[rgba(217,219,46,255)] border-[rgba(19,48,64,255)] box-border'>
-                <span className='text-xs pl-[10px] w-[30px] h-[10px] justify-center items-center'>$</span>
+                <span className='text-xs pl-[10px] w-[30px] h-[10px] justify-center items-center'>&pound;</span>
                 <input
                   value={amount}
                   onChange={handleInput(setAmount)}
@@ -87,7 +113,7 @@ function App() {
                     type="text"
                     name="rate"
                     id="rate"
-                    className="w-[80%] focus:outline-none h-8 border-[rgba(227,243,251,255)] p-2 rounded rounded-tr-none rounded-br-none mt-[7px]"
+                    className="w-[80%]  focus:outline-none h-8 border-[rgba(227,243,251,255)] p-2 rounded rounded-tr-none rounded-br-none mt-[7px]"
                   />
                   <span className='text-xs w-[20px] h-[10px] justify-center items-center'>%</span>
                 </div>
@@ -95,26 +121,28 @@ function App() {
             </div>
             <div className="flex flex-col h-auto mt-[15px]">
               <label htmlFor="mortgage-type" className="text-xs text-[#437087]">Mortgage Type</label>
-              <div className="flex w-full h-[40px] focus-within:bg-[#f5f5d1] rounded bg-white border-[rgba(19,48,64,255)] border-[1.2px] p-[10px] mt-[7px] focus-within:border-[rgba(217,219,46,255)]">
+              <div className="flex w-full h-[40px] cursor-pointer focus-within:bg-[#f5f5d1] rounded bg-white border-[rgba(19,48,64,255)] border-[1.2px] p-[10px] mt-[7px] focus-within:border-[rgba(217,219,46,255)]">
                 <input 
                 onChange={(e)=>{setMortgageType(e.target.value)}}
                 type="radio"
                 name='martgagetype' 
                  id="repayment"  
                 value="repayment" 
+                className='cursor-pointer'
                  required />
-                <label htmlFor="repayment" className="text-xs font-bold ml-[7px]">Repayment</label>
+                <label htmlFor="repayment" className="cursor-pointer text-xs font-bold ml-[7px]">Repayment</label>
               </div>
               
-              <div className="flex w-full h-[40px] focus-within:bg-[#f5f5d1] rounded bg-white border-[rgba(19,48,64,255)] border-[1.2px] p-[10px] mt-[7px] focus-within:border-[rgba(217,219,46,255)]">
+              <div className="cursor-pointer flex w-full h-[40px] focus-within:bg-[#f5f5d1] rounded bg-white border-[rgba(19,48,64,255)] border-[1.2px] p-[10px] mt-[7px] focus-within:border-[rgba(217,219,46,255)]">
                 <input 
                 onChange={(e)=>{setMortgageType(e.target.value)}}
                 type="radio" 
                 id="interest" 
                 name='martgagetype' 
                 value="interest" 
+                className='cursor-pointer ]'
                 required />
-                <label htmlFor="interest" className="text-xs font-bold ml-[7px]">Interest Only</label>
+                <label htmlFor="interest" className="cursor-pointer text-xs font-bold ml-[7px]">Interest Only</label>
               </div>
 
               <button
@@ -131,13 +159,30 @@ function App() {
           </form>
         </div>
 
-        <div className="w-full lg:w-1/2 h-full flex flex-col justify-center items-center lg:rounded-xl lg:rounded-tl-none lg:rounded-bl-[50px] bg-[rgba(19,48,64,255)] mt-6 lg:mt-0">
-          <img src="/illustration-empty.svg" className="w-40 h-40" alt="" />
-          <h2 className="text-white font-bold">Results shown here</h2>
-          <p className="text-center m-[20px] text-xs text-[#659ab4] font-static">
-            Complete the form and click “calculate repayments” to see what your monthly repayments would be.
-          </p>
-        </div>
+        
+        {!failedForm &&(
+              <div className="w-full lg:w-1/2 h-full flex flex-col justify-center items-center lg:rounded-xl lg:rounded-tl-none lg:rounded-bl-[50px] bg-[rgba(19,48,64,255)] mt-6 lg:mt-0">
+              <img src="/illustration-empty.svg" className="w-40 h-40" alt="" />
+              <h2 className="text-white font-bold">Results shown here</h2>
+              <p className="text-center m-[20px] text-xs text-[#659ab4] font-static">
+                Complete the form and click “calculate repayments” to see what your monthly repayments would be.
+              </p>
+              </div>
+        )}
+        {failedForm && (
+          <div className="w-full lg:w-1/2 h-full p-[20px] flex flex-col justify-center items-start lg:rounded-xl lg:rounded-tl-none lg:rounded-bl-[50px] bg-[rgba(19,48,64,255)] mt-6 lg:mt-0">
+            <h1 className='text-white'>Your results</h1>
+            <p className='text-start mt-[20px] mb-[20px] text-xs text-[#659ab4] font-static'>  Your results are shown below based on the information you provided. 
+                  To adjust the results, edit the form and click “calculate repayments” again.
+            </p>
+            <div className='p-[20px] w-[300px] h-150px border-t-[5px] border-t-[rgba(217,219,46,255)] rounded-[10px] bg-[#242d31]' >
+              <h4 className='text-[#659ab4] text-xs'>Your monthly repayments</h4>
+              <h1 className=' w-[] text-[rgba(217,219,46,255)] text-[50px] border-b-[1px] p-[5px] border-b-[#659ab4]'>&pound;{result}</h1>
+              <p className='text-[#659ab4] text-xs m-[10px]'> Total you'll repay over the term</p>
+              <p className='text-white font-bold'>&pound;{total}</p>
+            </div>
+            </div>
+        )}
       </div>
     </div>
   );
