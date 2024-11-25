84bad28e9cd27d305627d2851b8eb423fe9e97bc
khadija <khadijawakili652@gmail.com>
2024-11-25 22:52:28 +0430

    installed react

---
+++ product-list-with-cart-main/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",  // Tailwind will look for classes in all files inside src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
