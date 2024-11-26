02ff9d3400590ddc848f189a261ceca1ae32691a
khadija <khadijawakili652@gmail.com>
2024-11-26 12:16:35 +0430

    added the function add cart handler

--- product-list-with-cart-main/src/App.js
+++ product-list-with-cart-main/src/App.js
@@ -2,30 +2,42 @@ import logo from './logo.svg';
 import './App.css';
 import DessertCard from './components/DessertCard';
 
+
+
+
+
 function App() {
   const IMAGES=[
-    {img:"/assets/images/image-waffle-desktop.jpg" , name:" Waffle"},
-    {img:"/assets/images/image-waffle-desktop.jpg" , name:" Waffle"},
-    {img:"/assets/images/image-waffle-desktop.jpg" , name:" Waffle"},
-    {img:"/assets/images/image-waffle-desktop.jpg" , name:" Waffle"},
-    {img:"/assets/images/image-waffle-desktop.jpg" , name:" Waffle"},
-    {img:"/assets/images/image-waffle-desktop.jpg" , name:" Waffle"},
-    {img:"/assets/images/image-waffle-desktop.jpg" , name:" Waffle"},
-    {img:"/assets/images/image-waffle-desktop.jpg" , name:" Waffle"},
-    {img:"/assets/images/image-waffle-desktop.jpg" , name:" Waffle"},
+    {img:"/assets/images/image-waffle-desktop.jpg" , name:" Waffle" , discription:"Waffle with Berries",price:"6.50"},
+    {img:"/assets/images/image-creme-brulee-desktop.jpg" , name:"  Crème Brûlée" , discription:"Vanilla Bean Crème Brûlée",price:"7.00"},
+    {img:"/assets/images/image-macaron-desktop.jpg" , name:" macaron" , discription:"Waffle with Berries",price:"6.50"},
+    {img:"/assets/images/image-tiramisu-desktop.jpg" , name:" tiramisu" , discription:"Waffle with Berries",price:"6.50"},
+    {img:"/assets/images/image-baklava-desktop.jpg" , name:" baklava" , discription:"Waffle with Berries",price:"6.50"},
+    {img:"/assets/images/image-meringue-desktop.jpg" , name:" meringue" , discription:"Waffle with Berries",price:"6.50"},
+    {img:"/assets/images/image-cake-desktop.jpg" , name:" cake" , discription:"Waffle with Berries",price:"6.50"},
+    {img:"/assets/images/image-brownie-desktop.jpg" , name:" brownie" , discription:"Waffle with Berries",price:"6.50"},
+    {img:"/assets/images/image-panna-cotta-desktop.jpg" , name:" panna" , discription:"Waffle with Berries",price:"6.50"},
   ]
+
+  
   return (
-    <div className="flex flex-row w-screen h-screen justify-between">
-      <section className='w-[600px] h-screen grid  grid-cols-3 grid-rows-3 gap-1 ml-32 p-5'>
+    <div className="flex flex-row w-screen h-auto  justify-between">
+      
+      <section className='w-[600px] flex flex-col mt-10'>
+      <h1 className='font-bold text-3xl ml-40'>Desserts</h1>
+      <div className='w-[700px]  grid  grid-cols-3 grid-rows-3 ml-32 p-5'>
       {IMAGES.map(card=>(
         <DessertCard
+        price={card.price}
+        discription={card.discription}
         image={card.img}
         name={card.name}
         />
 
       ))}
+      </div>
       </section>
-      <section className='w-[400px] h-screen bg-slate-600'>
+      <section className='w-[400px]  bg-slate-600'>
 
       </section>
     </div>
--- product-list-with-cart-main/src/components/DessertCard.js
+++ product-list-with-cart-main/src/components/DessertCard.js
@@ -1,17 +1,55 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
-const DessertCard = ({image,name,price}) => {
+const DessertCard = ({ image, name, price, discription }) => {
+  const [isAddcartClicked, setIsAddCartClicked] = useState(true);
+  const [addToCart , setAddToCart]=useState("")
+
+  const handleAdd=()=>{
+    setIsAddCartClicked(!isAddcartClicked)
+  }
   return (
     <div>
-      <div className='w-[150px] h-[200px] bg-white flex flex-col justify-center items-center'>
-        <img
-        className='w-[150px] h-[150px]'
-         src={image} alt="" />
-        <p>{name}</p>
-        <p>{price}</p>
+      <div className="w-[200px] h-[260px] m-2 bg-white flex flex-col justify-center items-center">
+        <div className=" relative w-[170px] h-[170px] flex justify-center items-center cursor-pointer ">
+          <img className="w-[170px] h-[170px] rounded-md" src={image} alt="" />
+
+          {isAddcartClicked ?
+          (<div className="flex flex-row justify-center bottom-[-20px]  bg-white items-center w-[120px] absolute h-[30px] p-2  space-x-2 rounded-3xl border-[1px] border-black ">
+            <img
+              src="/assets/images/icon-add-to-cart.svg"
+              alt="the add to cadt icon"
+            />
+            <span
+            onClick={handleAdd}
+             className="text-xs  ">Add to cart</span>
+          </div>
+          ):(
+            <div className="flex flex-row justify-center bottom-[-20px]  bg-red-700 items-center w-[120px] absolute h-[30px] p-2  space-x-6 rounded-3xl  ">
+            <img
+            className= "hover:bg-white hover:text-red-700 fill-transparent  border-white border-[1px] w-4 h-4 p-1 flex justify-center items-center rounded-full "
+              src="/assets/images/icon-decrement-quantity.svg"
+              alt="the add to cadt icon"
+            />
+            <span className="text-xs text-white ">0</span>
+            <img
+             className="border-white border-[1px] w-4 h-4 p-1 flex justify-center items-center rounded-full"
+              src="/assets/images/icon-increment-quantity.svg"
+              alt="the add to cadt icon"
+            />
+          </div>
+          )}
+        </div>
+        
+
+        <div className="h-[200px] w-[170px] mt-6">
+          <p className="text-xs text-gray-500 m-1">{name}</p>
+          <p className="text-xs font-bold m-1">{discription}</p>
+          <p className="text-red-700 text-xs font-bold m-1">{price}</p>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default DessertCard
+export default DessertCard;
