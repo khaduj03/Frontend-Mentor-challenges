b56b3f0105c638d01b97993fbe3afe51612ec722
khadija <khadijawakili652@gmail.com>
2024-11-26 18:02:07 +0430

    Add handleDeleteCart function for delete the order

--- product-list-with-cart-main/src/components/DessertCard.js
+++ product-list-with-cart-main/src/components/DessertCard.js
@@ -1,27 +1,90 @@
 import React from "react";
 import { FaPlus, FaMinus } from 'react-icons/fa';
+import { motion } from 'framer-motion';
+import { AnimatePresence } from 'framer-motion';
+import { useState } from "react";
+const DessertCard = ({num,setOrderCart,setDesserts,cartId,count, setNum,idCart, image, name, price, discription, handleAddCart, isAdded }) => {
 
-const DessertCard = ({ id, image, name, price, discription, handleAddCart, isAdded }) => {
+    const handleIncrement = (id) => {
+        setOrderCart(prev => {
+          return prev.map(item => {
+            if (item.id === id) {
+              return { ...item, count: (item.count || 0) + 1 };
+            }
+            return item;
 
+          });
+        });
+
+        setDesserts(prev=>{
+            return prev.map(item => {
+                if (item.id === id) {
+                  return { ...item, count: (item.count || 0) + 1 };
+                }
+                return item;
+    
+              });
+        })
+      };
+      
+
+    const handleDecrement=(id)=>{
+        setOrderCart(prev => {
+            return prev.map(item => {
+              if (item.id === id &&item.count!==1) {
+                return { ...item, count: (item.count || 1) -1 };
+              }
+              return item;
+  
+            });
+          });
+  
+          setDesserts(prev=>{
+              return prev.map(item => {
+                  if (item.id === id &&item.count!==1) {
+                    return { ...item, count: (item.count || 1) - 1 };
+                  }
+                  return item;
+      
+                });
+          })
+    }
   return (
     <div>
       <div className="w-[200px] h-[260px] m-2 bg-pink-50  flex flex-col justify-center items-center">
         <div className="relative w-[170px] h-[170px] flex justify-center items-center cursor-pointer">
-          <img className="w-[170px] h-[170px] rounded-md border-red-700 border" src={image} alt={name} />
+          <img className={`${isAdded ? "border-red-700 border-[2px] shadow-[0px_5px_60px_rgba(0,0,0,0.2)] " : "border-none"} w-[170px] h-[170px] rounded-md`} src={image} alt={name} />
 
           {!isAdded && (
             <div className="flex flex-row justify-center bottom-[-20px] bg-white items-center w-[120px] absolute h-[30px] p-2 space-x-2 rounded-3xl border-[1px] border-black">
               <img src="/assets/images/icon-add-to-cart.svg" alt="Add to cart icon" />
-              <span onClick={() => handleAddCart(id, image, price, name)} className="text-xs">Add to cart</span>
+              <span onClick={() => handleAddCart(idCart, image, price, name,count)} className="text-xs">Add to cart</span>
             </div>
           )}
 
           {isAdded && (
-            <div className="flex flex-row text-white justify-center bottom-[-20px] bg-red-700 items-center w-[120px] absolute h-[30px] p-2 space-x-6 rounded-3xl">
-              <FaMinus className="hover:bg-white hover:text-red-700 border-white border-[1px] w-4 h-4 p-1 flex justify-center items-center rounded-full" />
-              <span className="text-xs text-white">1</span> {/* تعداد اقلام در سبد خرید */}
-              <FaPlus className="border-white hover:bg-white hover:text-red-700 text-white border-[1px] w-4 h-4 p-1 flex justify-center items-center rounded-full" />
-            </div>
+            <motion.div className="flex flex-row text-white justify-center bottom-[-20px] bg-red-700 items-center w-[120px] absolute h-[30px] p-2 space-x-6 rounded-3xl 
+            "
+            initial={{ opacity: 0 }}
+            animate={{opacity: 1 }}
+            exit={{ opacity: 0 }}
+            
+            >
+              <FaMinus 
+              onClick={()=>{handleDecrement(idCart)}}
+              className="hover:bg-white hover:text-red-700 border-white border-[1px] w-4 h-4 p-1 flex justify-center items-center rounded-full" />
+              <AnimatePresence>
+              <motion.span 
+            initial={{y:20, opacity: 0 }}
+            animate={{y:0,opacity: 1 }}
+            exit={{y:-20, opacity: 0 }}
+              className="text-xs text-white">{count}</motion.span> 
+              </AnimatePresence>
+              <FaPlus 
+              onClick={()=>handleIncrement(idCart)}
+            //   onClick={handleIncrement}
+              className="border-white hover:bg-white hover:text-red-700 text-white border-[1px] w-4 h-4 p-1 flex justify-center items-center rounded-full" />
+            </motion.div>
           )}
         </div>
 
--- product-list-with-cart-main/src/components/OrderCard.js
+++ product-list-with-cart-main/src/components/OrderCard.js
@@ -1,21 +1,43 @@
 import React from 'react';
+import {  motion } from 'framer-motion';
+import { AnimatePresence } from 'framer-motion';
+import { FaTimes } from 'react-icons/fa';
 
-const OrderCard = ({ orderCart }) => {
+const OrderCard = ({ orderCart ,setOrderCart}) => {
+    const handleDelete=(id)=>{
+        setOrderCart(prev => prev.filter(item => item.id !== id));
+    }
   return (
     <div>
-      <div className='bg-white w-[330px] rounded-xl mr-32'>
+      <div className='bg-white  w-[330px] h-[310px] overflow-x-hidden  overflow-y-auto rounded-xl mr-32 mt-12'>
         <h1 className='text-red-700 font-bold text-2xl w-full h-11 justify-start ml-4 items-center flex '>Your Cart</h1>
-        {/* Iterate through the orderCart array and render each item */}
         {orderCart.map(item => {
           return (
-            <div key={item.id} className='flex flex-row m-5 '>
+            <AnimatePresence>
+            <motion.div
+            initial={{ x: 100, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            exit={{ x: -100, opacity: 0 }}
+            >
+            <div key={item.id} className='flex justify-between items-center flex-row m-2 p-2 shadow-[0px_8px_20px_rgba(0,0,0,0.2)] rounded-lg '>
+            <div className='flex flex-row'>
              <img 
               className='w-[60px] h-[60px] rounded-lg'
               src={item.img} alt={item.name} />
-              <p>{item.name}</p>
-              <p>{item.price}</p>
-
+              <div className='ml-3'>
+              <p className='font-bold '>{item.name}</p>
+              <p className='text-gray-500 text-xs '><span className='text-red-700 font-bold' >x{item.count} </span> @ ${item.price}  <span> ${(item.count)*(item.price)}</span></p>
+              </div>
+              </div>
+              <div>
+                <FaTimes
+                onClick={()=>{handleDelete(item.id)}}
+                className='text-xs cursor-pointer  mr-2  border-[1px] border-black rounded-full '
+                />
+              </div>
             </div>
+            </motion.div>
+            </AnimatePresence>
           );
         })}
       </div>
