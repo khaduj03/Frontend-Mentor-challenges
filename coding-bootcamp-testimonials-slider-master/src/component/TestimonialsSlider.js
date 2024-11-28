027e29cd2b51734b6a8f91163b0604f157366cee
khadija <khadijawakili652@gmail.com>
2024-11-28 22:35:43 +0430

    installed tailwaind css

---
+++ coding-bootcamp-testimonials-slider-master/src/component/TestimonialsSlider.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TestimonialsSlider = ({image}) => {
+  return (
+    <div>
+      <div>
+        <img src={image} alt={image} />
+      </div>
+    </div>
+  )
+}
+
+export default TestimonialsSlider;
