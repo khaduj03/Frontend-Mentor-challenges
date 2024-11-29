52292bad3460a5750b7c2229583bdae5f0f00d6f
khadija <khadijawakili652@gmail.com>
2024-11-29 11:09:48 +0430

    Add Slider component

--- coding-bootcamp-testimonials-slider-master/src/component/TestimonialsSlider.js
+++ coding-bootcamp-testimonials-slider-master/src/component/TestimonialsSlider.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React from "react";
 
-const TestimonialsSlider = ({image}) => {
+const TestimonialsSlider = ({ image, text }) => {
   return (
-    <div>
-      <div>
-        <img src={image} alt={image} />
+    <div className="flex  w-4/5 h-4/5 justify-center relative ">
+        <img
+        className="absolute z-20 w-96 right-0"
+         src="images/pattern-bg.svg" alt="bg-svg" />
+        
+      <div className="text-3xl  w-96">
+        <p className="flex w-96 absolute z-40 left-0">{text}</p>
+      </div>
+      <div className="w-[300px] h-[350px] relative">
+        <img className="w-5/6 absolute z-30 right-0 " src={image} />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default TestimonialsSlider;
