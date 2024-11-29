5ce6ac9ca267612452b6b17cefc77a55c7f52ab3
khadija <khadijawakili652@gmail.com>
2024-11-29 13:25:48 +0430

    finished project

--- coding-bootcamp-testimonials-slider-master/src/App.js
+++ coding-bootcamp-testimonials-slider-master/src/App.js
@@ -1,9 +1,9 @@
 import { useState } from "react";
+import { AnimatePresence } from "framer-motion"; // Import AnimatePresence
 import "./App.css";
 import TestimonialsSlider from "./component/TestimonialsSlider";
+
 function App() {
-
-
   const data = [
     {
       id: 1,
@@ -18,43 +18,46 @@ function App() {
       id: 2,
       image: "/images/image-tanya.jpg",
       text: `  “ I’ve been interested in coding for a while but never taken the jump, until now. 
-  I couldn’t recommend this course enough. I’m now in the job of my dreams and so 
-  excited about the future. ”`,
+  I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”`,
       name: "Tanya Sinclair",
       skill: "UX Engineer",
     },
   ];
 
-  const [ currentIndex , setCurrentIndex]=useState(0)
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-  const handleSlider=()=>{
-    if(currentIndex<data.length-1){
-      setCurrentIndex(currentIndex+1)
+  const handleSlider = () => {
+    if (currentIndex < data.length - 1) {
+      setCurrentIndex(currentIndex + 1);
     }
-  }
+  };
 
-  const handlePrevSlider=()=>{
-    if(currentIndex>0){
-      setCurrentIndex(currentIndex-1)
+  const handlePrevSlider = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex(currentIndex - 1);
     }
-  }
-
-
+  };
 
   return (
-    <div className="">
-      <div className="w-screen h-screen flex justify-center items-center">
-        <TestimonialsSlider
+    <div className="App">
+      <main className="w-screen h-screen flex justify-center lg:p-20 items-center">
+        <AnimatePresence>
+          <TestimonialsSlider
+            key={currentIndex}
             image={data[currentIndex].image}
             text={data[currentIndex].text}
             handlePrevSlider={handlePrevSlider}
             handleSlider={handleSlider}
-         />
-         <img 
-         className="absolute z-10 bottom-0 left-0"
-         src="/images/pattern-curve.svg" 
-         alt="curve svg" />
-      </div>
+            name={data[currentIndex].name}
+            skill={data[currentIndex].skill}
+          />
+        </AnimatePresence>
+        <img
+          className="absolute z-10 bottom-0 left-0"
+          src="/images/pattern-curve.svg"
+          alt="curve svg"
+        />
+      </main>
     </div>
   );
 }
