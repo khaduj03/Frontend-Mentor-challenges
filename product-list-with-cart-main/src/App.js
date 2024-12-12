e63e114cad857fb0b7788ae07cd86c9348a41916
khadija <khadijawakili652@gmail.com>
2024-12-12 07:53:12 +0430

    Updated the code

--- product-list-with-cart-main/src/App.js
+++ product-list-with-cart-main/src/App.js
@@ -4,87 +4,12 @@ import DessertCard from "./components/DessertCard";
 import OrderCard from "./components/OrderCard";
 import ModalOrder from "./components/ModalOrder";
 import { motion } from "framer-motion";
+import { IMAGES } from "./detailsCart/details";
 
 function App() {
-  const IMAGES = [
-    {
-      id: 1,
-      img: "/assets/images/image-waffle-desktop.jpg",
-      name: " Waffle",
-      discription: "Waffle with Berries",
-      price: 6.5,
-      count: 1,
-    },
-    {
-      id: 2,
-      img: "/assets/images/image-creme-brulee-desktop.jpg",
-      name: "  Crème Brûlée",
-      discription: "Vanilla Bean Crème Brûlée",
-      price: 7.0,
-      count: 1,
-    },
-    {
-      id: 3,
-      img: "/assets/images/image-macaron-desktop.jpg",
-      name: " Macaron",
-      discription: " Macaron Mix of Five",
-      price: 8.0,
-      count: 1,
-    },
-    {
-      id: 4,
-      img: "/assets/images/image-tiramisu-desktop.jpg",
-      name: "  Tiramisu",
-      discription: " Classic Tiramisu",
-      price: 5.5,
-      count: 1,
-    },
-    {
-      id: 5,
-      img: "/assets/images/image-baklava-desktop.jpg",
-      name: "  Baklava",
-      discription: " Pistachio Baklava",
-      price: 4.0,
-      count: 1,
-    },
-    {
-      id: 6,
-      img: "/assets/images/image-meringue-desktop.jpg",
-      name: "Pie",
-      discription: " Lemon Meringue Pie",
-      price: 5.0,
-      count: 1,
-    },
-    {
-      id: 7,
-      img: "/assets/images/image-cake-desktop.jpg",
-      name: " Cake",
-      discription: " Red Velvet Cake",
-      price: 4.5,
-      count: 1,
-    },
-    {
-      id: 8,
-      img: "/assets/images/image-brownie-desktop.jpg",
-      name: " Brownie",
-      discription: " Salted Caramel Brownie",
-      price: 4.5,
-      count: 1,
-    },
-    {
-      id: 9,
-      img: "/assets/images/image-panna-cotta-desktop.jpg",
-      name: " Panna Cotta",
-      discription: "Vanilla Panna Cotta",
-      price: 6.5,
-      count: 1,
-    },
-  ];
-
   const [orderCart, setOrderCart] = useState([]);
   const [isEmpty, setIsempty] = useState(false);
   const [desserts, setDesserts] = useState(IMAGES);
-  const [cartId, setCartId] = useState(null);
   const [count, setCount] = useState(0);
   const [showModal, setShowModal] = useState(false);
 
@@ -112,7 +37,6 @@ function App() {
               setCount={setCount}
               setDesserts={setDesserts}
               setOrderCart={setOrderCart}
-              cartId={cartId}
               orderCart={orderCart}
               key={card.id}
               idCart={card.id}
